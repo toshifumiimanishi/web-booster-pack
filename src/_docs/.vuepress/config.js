50b5f41cab545dcf58215a52706549688e913926
Imanishi Toshifumi <13635766+toshifumiimanishi@users.noreply.github.com>
2019-01-14 23:36:31 +0900

    :pencil2: Docs: ドキュメントの新規作成

---
+++ src/_docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'Website Boilerplate',
+  dest: 'docs',
+  locales: {
+    '/': {
+      lang: 'ja'
+    }
+  },
+  themeConfig: {
+    nav: [
+      { text: 'Components', link: '/components/'}
+    ],
+    sidebar: [
+      '/components/',
+    ]
+  }
+}
