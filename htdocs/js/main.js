93b0e7cb54580d90627b9907a4b3e799ecf8a7d0
Imanishi Toshifumi <13635766+toshifumiimanishi@users.noreply.github.com>
2019-03-10 19:53:59 +0900

    :art: Add: モーダルコンポーネントの追加

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -1,1 +1,59 @@
 "use strict";
+
+var modal = function () {
+  var toggleButton = document.querySelectorAll('.js-toggle-modal');
+  var currentScrollY = 0;
+  toggleButton.forEach(function (element) {
+    var target = document.querySelector(element.dataset.target);
+    var dialog = target.querySelector('[role="document"]');
+    var dismissButton = target.querySelectorAll('[data-dismiss="modal"]');
+    element.addEventListener('click', function () {
+      _launch(target);
+    });
+    dismissButton.forEach(function (element) {
+      element.addEventListener('click', function () {
+        _dismiss(target);
+      });
+    });
+    target.addEventListener('click', function () {
+      _dismiss(target);
+    });
+    dialog.addEventListener('click', function (event) {
+      event.stopPropagation();
+    });
+    element.addEventListener('keydown', function (event) {
+      var escKeyCode = 27;
+
+      if (event.keyCode === escKeyCode) {
+        _dismiss(target);
+      }
+    });
+
+    function _launch(element) {
+      _activateScrollLock();
+
+      element.setAttribute('aria-hidden', 'false');
+    }
+
+    function _dismiss(element) {
+      _deactivateScrollLock();
+
+      element.setAttribute('aria-hidden', 'true');
+    }
+
+    function _activateScrollLock() {
+      var scrollbarWidth = window.innerWidth - document.body.clientWidth;
+      currentScrollY = window.scrollY;
+      document.body.style.position = 'fixed';
+      document.body.style.top = "-".concat(currentScrollY, "px");
+      document.body.style.right = '0';
+      document.body.style.left = '0';
+      document.body.style.paddingRight = "".concat(scrollbarWidth, "px");
+    }
+
+    function _deactivateScrollLock() {
+      document.body.setAttribute('style', '');
+      window.scrollTo(0, currentScrollY);
+    }
+  });
+}();
---
+++ src/_js/js/main.js
@@ -0,0 +1,62 @@
+const modal = (function() {
+  const toggleButton = document.querySelectorAll('.js-toggle-modal');
+  let currentScrollY = 0;
+
+  toggleButton.forEach(element => {
+    const target = document.querySelector(element.dataset.target);
+    const dialog = target.querySelector('[role="document"]');
+    const dismissButton = target.querySelectorAll('[data-dismiss="modal"]');
+
+    element.addEventListener('click', () => {
+      _launch(target);
+    });
+
+    dismissButton.forEach(element => {
+      element.addEventListener('click', () => {
+        _dismiss(target);
+      });
+    });
+
+    target.addEventListener('click', () => {
+      _dismiss(target);
+    });
+
+    dialog.addEventListener('click', event => {
+      event.stopPropagation();
+    });
+
+    element.addEventListener('keydown', event => {
+      const escKeyCode = 27;
+
+      if (event.keyCode === escKeyCode) {
+        _dismiss(target);
+      }
+    });
+
+    function _launch(element) {
+      _activateScrollLock();
+      element.setAttribute('aria-hidden', 'false');
+    }
+
+    function _dismiss(element) {
+      _deactivateScrollLock();
+      element.setAttribute('aria-hidden', 'true');
+    }
+
+    function _activateScrollLock() {
+      const scrollbarWidth = window.innerWidth - document.body.clientWidth;
+      currentScrollY = window.scrollY;
+
+      document.body.style.position = 'fixed';
+      document.body.style.top = `-${currentScrollY}px`;
+      document.body.style.right = '0';
+      document.body.style.left = '0';
+      document.body.style.paddingRight = `${scrollbarWidth}px`;
+    }
+
+    function _deactivateScrollLock() {
+      document.body.setAttribute('style', '');
+      window.scrollTo(0, currentScrollY);
+    }
+  });
+})();
