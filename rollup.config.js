fc78a59627b1f4b0a23537ad86e8562c966d333b
Imanishi Toshifumi <13635766+toshifumiimanishi@users.noreply.github.com>
2020-03-20 14:40:40 +0900

    🆙 Update: rollup-plugin-node-resolve → @rollup/plugin-node-resolve に変更（#12）

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import resolve from 'rollup-plugin-node-resolve';
+import resolve from '@rollup/plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import babel from 'rollup-plugin-babel';
