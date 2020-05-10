924ee2e58de32a44cea9cf4f307ebad073b08502
toshifumiimanishi <13635766+toshifumiimanishi@users.noreply.github.com>
2020-05-10 13:57:12 +0900

    :+1: Update: Rollup 監視下のバンドルファイルを ESLint で静的検証する

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import babel from 'rollup-plugin-babel';
+import { eslint } from 'rollup-plugin-eslint';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -15,6 +16,7 @@ export default (async () => ({
   plugins: [
     resolve(),
     commonjs(),
+    eslint(),
     typescript(),
     babel(),
     isProduction && (await import('rollup-plugin-terser')).terser()
